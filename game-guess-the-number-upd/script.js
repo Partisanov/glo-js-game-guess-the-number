f09cc635714e2eed0f9b5a103c10c1aeebf1a534
Konstantin Pozharitskiy <tripless@mail.ru>
2022-10-06 23:58:37 +0300

    rework diffucult task

--- game-guess-the-number-upd/script.js
+++ game-guess-the-number-upd/script.js
@@ -6,7 +6,7 @@ const gameGuessNumber = () => {
 
     //блок объявления переменных 
     const findNumber = Math.floor(Math.random() * 100) + 1;
-    let attempts = 10;
+    let attemps = 10;
 
     //блок описания функций
     const sendMessage = (message) => {
@@ -21,52 +21,42 @@ const gameGuessNumber = () => {
         return prompt('Введите число от 1 до 100:');
     };
 
-    const getUserNumber = () => {
-        let userAnswer = getUserAnswer();
-        let userNumber;
-        if (userAnswer === null) {
-            sendMessage('Игра окончена!');
-            rePlay('Желаете сыграть еще раз?');
-        } else if (!isNumber(userAnswer)) {
-            sendMessage('Введи число!');
-            userNumber = getUserNumber();
-        } else if (+userAnswer < 0 || +userAnswer > 100) {
-            sendMessage('Число должно быть от 1 до 100');
-            userNumber = getUserNumber();
-        } else {
-            userNumber = +userAnswer;
-        }
-        if (userNumber) {
-            return (userNumber);
-        }
-    };
-
-    const getMatchCheck = (number) => {
-        if (number === findNumber) {
-            rePlay('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
-        } else if (attempts === 0) {
-            rePlay('Попытки закончились,хотите сыграть еще?');
-        } else if (number < findNumber) {
-            sendMessage('Загаданное число больше, осталось попыток ' + attempts);
-            attempts--;
-            getMatchCheck(getUserNumber());
-        } else if (number > findNumber) {
-            sendMessage('Загаданное число меньше, осталось попыток ' + attempts);
-            attempts--;
-            getMatchCheck(getUserNumber());
-        }
-    };
-
-    const gamePlay = () => {
-        getMatchCheck(getUserNumber());
-    };
-
     const rePlay = (message) => {
         const answer = confirm(message);
         if (answer === false) {
             sendMessage('До свидания!');
+            return;
         } else {
             gameGuessNumber();
+        }
+    };
+
+    const gamePlay = () => {
+        attemps--;
+        if (attemps < 0) {
+            rePlay('Попытки закончились, хотите сыграть еще?');
+        } else {
+            let userAnswer = getUserAnswer();
+            if (userAnswer === null) {
+                sendMessage('Игра окончена!');
+                return;
+            }
+            if (isNumber(userAnswer)) {
+                const userNumber = +userAnswer;
+                if (userNumber > findNumber) {
+                    sendMessage(`Загаданное число меньше, осталось попыток: ${attemps}`);
+
+                    gamePlay();
+                } else if (userNumber < findNumber) {
+                    sendMessage(`Загаданное число больше, осталось попыток: ${attemps}`);
+                    gamePlay();
+                } else {
+                    rePlay('Поздравляю, Вы угадали!!!Хотели бы сыграть еще?');
+                }
+            } else {
+                sendMessage('Введи число!');
+                gamePlay();
+            }
         }
     };
 
